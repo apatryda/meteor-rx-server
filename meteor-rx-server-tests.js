f6e827925d144279fb30f79a6501650babcffdab
apatryda <a.apatryda@gmail.com>
2018-04-05 03:35:51 +0200

    fixed test

--- meteor-rx-server-tests.js
+++ meteor-rx-server-tests.js
@@ -1,11 +1,11 @@
 // Import Tinytest from the tinytest Meteor package.
-import { Tinytest } from "meteor/tinytest";
+import { Tinytest } from 'meteor/tinytest';
 
 // Import and rename a variable exported by meteor-rx-server.js.
-import { name as packageName } from "meteor/meteor-rx-server";
+import { name as packageName } from 'meteor/apatryda:meteor-rx-server';
 
 // Write your tests here!
 // Here is an example.
 Tinytest.add('meteor-rx-server - example', function (test) {
-  test.equal(packageName, "meteor-rx-server");
+  test.equal(packageName, 'apatryda:meteor-rx-server');
 });
